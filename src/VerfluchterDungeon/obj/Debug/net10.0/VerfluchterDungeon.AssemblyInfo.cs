2e5142f0ec61c9901a50cca7d61c4888d45c0232
Edo017 <E.Omercevic.inf25@stud.bbbaden.ch>
2026-08-28 07:29:59 +0200

    edo

--- src/VerfluchterDungeon/obj/Debug/net10.0/VerfluchterDungeon.AssemblyInfo.cs
+++ src/VerfluchterDungeon/obj/Debug/net10.0/VerfluchterDungeon.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VerfluchterDungeon")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+640f55b5bfd484abf6eed3defbb281676c68495c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8462076c068bd09b7c8bdc100a1f3127e52a41f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("VerfluchterDungeon")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VerfluchterDungeon")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
